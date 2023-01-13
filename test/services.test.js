49da2e37d19608d1b7100232e30c756299cfced8
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-01-13 15:14:48 +0100

    feat: services.tests - testing getLastEvent

--- test/services.test.js
+++ test/services.test.js
@@ -21,10 +21,10 @@ describe("Event Service",()=> {
     let fakeEvents = [
         new Event(new Date('2019-12-17T03:24:00'),new Date('2019-12-17T13:24:00'),"Hello World","Campus Numerique","This is an hello world.."),
         new Event(new Date('2018-12-17T03:24:00'),new Date('1995-12-17T03:24:00'),"First event","Campus Numerique","This is an hello world.."),
-        new Event(new Date('2020-04-01T09:00:00'),new Date('2020-04-01T17:00:00'),"Unit test againt","Campus Numerique","This is an hello world..")
+        new Event(new Date('2020-04-01T09:00:00'),new Date('2020-04-01T17:00:00'),"Unit test again","Campus Numerique","This is an hello world..")
     ];
 
-    // getEvents
+    // 1 - getEvents
     test('getEvents shall call repository', async () => {
         let eventService = new EventService(new EventRepository());
         eventService.getEvents();
@@ -36,7 +36,7 @@ describe("Event Service",()=> {
         expect(eventService.getEvents().length).toBe(3);
     })
 
-    // getFirstEvent
+    // 2 - getFirstEvent
     test('getFirstEvent shall return 1 result', async () => {
         let eventService = new EventService(new EventRepository());
         expect(eventService.getFirstEvent()).not.toBeInstanceOf(Array);
@@ -49,8 +49,29 @@ describe("Event Service",()=> {
         let eventService = new EventService(new EventRepository());
         expect(eventService.getFirstEvent()).toBe(fakeEvents[0]);
     })
+
+    // 3 - getLastEvent en TDD
+    test('getLastEvent shall call repository', async () => {
+        let eventService = new EventService(new EventRepository());
+        eventService.getLastEvent();
+        expect(EventRepository).toHaveBeenCalledTimes(1);
+    })
+    test('getLastEvent shall return an instance of Event', async () => {
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getLastEvent()).toBeInstanceOf(Event);
+    })
+    test('getLastEvent shall return the first event of fakeEvents', async () => {
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getLastEvent()).toBe(fakeEvents[fakeEvents.length-1]);
+    })
+    test('getLastEvent shall return the first event of fakeEvents', async () => {
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getLastEvent().description).toBe("Unit test again");
+    })
 });
 
+
+// Nouveau contexte: base de donnée vide
 describe("Event Service",()=> {
 
     beforeEach(() => {
@@ -63,13 +84,18 @@ describe("Event Service",()=> {
             }
         });
     });
-    // jest envoie des fausses données de repository au test
     let fakeEvents = [];
 
-    // getFirstEvent
+    // 2 - getFirstEvent
     test('getFirstEvent shall return empty object if no events are in DB', async () => {
         let eventService = new EventService(new EventRepository());
         expect(eventService.getFirstEvent())=={};
+    })
+
+    // 3 - getLastEvent
+    test('getLastEvent shall return empty object if no events are in DB', async () => {
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getLastEvent())=={};
     })
 
 
